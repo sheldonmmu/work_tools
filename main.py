4d325f3c14d3a73ddb40effc6cf4cf4c3b640cbd
sheldonmmu <s.korpet@mmu.ac.uk>
2025-08-14 16:01:51 +0100

    update css on main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# Read the CSS file
-with open("styles.css") as f:
-    css = f.read()
-
-# Inject CSS with st.markdown
-st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+##STYLE
+# set in config.toml
 
 st.title("Sheldon's Tools and Experiments")
 st.image('https://miro.medium.com/v2/resize:fit:1100/format:webp/1*yvRmV4yeH8YwSpJpgqIxpA.png', caption='Image Caption')
